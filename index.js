3a3a5e1ba91bbbc32e5a0bd6b53e88f69d141331
Joaquin Blanco <blancojoaquin77@gmail.com>
2024-03-08 18:56:16 -0300

    hay un bug

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+const tamanioMaximo = 32;
 let microfono;
 let mostrarTexto = true;
-let tamanioTexto = 0
-const tamaniomaximo = 72
+let tamanioGrita = tamanioMaximo;
+let tamanioFrase = 0;
 
 function setup() {
   let canvas = createCanvas(400, 400, WEBGL);
@@ -10,34 +11,29 @@ function setup() {
   userStartAudio();
   microfono = new p5.AudioIn();
   microfono.start();
-  frameRate (60)
+  frameRate(60);
 }
 
 function draw() {
   let gritoNivel = microfono.getLevel();
   // Ajusta la visibilidad de "Grita" en función del nivel de audio
-  mostrarTexto = gritoNivel <= 0.1
+  mostrarTexto = gritoNivel <= 0.1;
   text("activar", width / 2, 20);
 
-  if (tamanioTexto < tamaniomaximo & mostrarTexto) {
+  if ((tamanioFrase < tamanioMaximo) & mostrarTexto) {
     // Ajusta el tamaño de "Grita" en función del nivel de audio
-    tamanioTexto = map(gritoNivel, 0, 1, 1, tamaniomaximo); // Tamaño se ajusta de 1 a 0
-    document.getElementById("grita").style.fontSize = tamanioTexto + "em";
-
-
+    tamanioGrita = map(gritoNivel, 0, 1, tamanioMaximo, 0); // Tamaño se ajusta de 1 a 0
+    document.getElementById("grita").style.fontSize = tamanioGrita + "em";
 
     // Ajusta la visibilidad del texto "Grita"
-    document.getElementById("frase").style.visibility = mostrarTexto
-      ? "hidden"
-      : "visible";
+    tamanioFrase = map(gritoNivel, 0, 1, 0, tamanioMaximo); // Tamaño se ajusta de 1 a 0
+    document.getElementById("frase").style.fontSize = tamanioFrase + "em";
+  } else {
+    tamanioFrase = tamanioFrase < 0 ? 0 : tamanioFrase - 1;
+    document.getElementById("frase").style.fontSize = tamanioFrase + "em";
 
+    tamanioGrita =
+      tamanioGrita > tamanioMaximo ? tamanioMaximo : tamanioGrita + 1;
+    document.getElementById("grita").style.fontSize = tamanioGrita + "em";
   }
-  else {
-    tamanioTexto = tamanioTexto - 1
-    document.getElementById("grita").style.fontSize = tamanioTexto + "em";
-
-  }
-
-
-
 }
