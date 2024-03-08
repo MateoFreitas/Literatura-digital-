6f4b7d5e8a4ff15fd796e77c864764732e70159e
nahuelargentina <alanledesma3279@gmail.com>
2024-03-08 18:23:34 -0300

    push FINAL

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 let microfono;
 let mostrarTexto = true;
-
-
+let tamanioTexto = 0
+const tamaniomaximo = 20
 
 function setup() {
   let canvas = createCanvas(400, 400, WEBGL);
-  
+
   canvas.mousePressed(userStartAudio);
   userStartAudio();
   microfono = new p5.AudioIn();
@@ -15,20 +15,26 @@ function setup() {
 function draw() {
   let gritoNivel = microfono.getLevel();
   // Ajusta la visibilidad de "Grita" en función del nivel de audio
-  mostrarTexto = gritoNivel<=0.1
-  text("activar",width / 2,20 );
- 
+  mostrarTexto = gritoNivel <= 0.1
+  text("activar", width / 2, 20);
 
-  // Ajusta el tamaño de "Grita" en función del nivel de audio
-  let tamanioTexto = map(gritoNivel, 0, 1, 1, 10); // Tamaño se ajusta de 1 a 0
-  document.getElementById("grita").style.fontSize = tamanioTexto + "em";
+  if (tamanioTexto < tamaniomaximo & mostrarTexto) {
+    // Ajusta el tamaño de "Grita" en función del nivel de audio
+    tamanioTexto = map(gritoNivel, 0, 1, 1, tamaniomaximo); // Tamaño se ajusta de 1 a 0
+    document.getElementById("grita").style.fontSize = tamanioTexto + "em";
 
 
 
-  // Ajusta la visibilidad del texto "Grita"
-  document.getElementById("frase").style.visibility = mostrarTexto 
-    ? "hidden"
-    : "visible";
+    // Ajusta la visibilidad del texto "Grita"
+    document.getElementById("frase").style.visibility = mostrarTexto
+      ? "hidden"
+      : "visible";
 
-  sleep(1000)
+  }
+  else {
+    tamanioTexto = tamanioTexto - 1
+  }
+
+
+
 }
