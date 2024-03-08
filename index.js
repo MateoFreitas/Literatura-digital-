da0a0b0232ee8b9da8f0bb8a8a43a9612a2bfcb1
nahuelargentina <alanledesma3279@gmail.com>
2024-03-08 18:11:00 -0300

    push con sleep

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 let microfono;
 let mostrarTexto = true;
 
+
+
 function setup() {
-  let canvas = createCanvas(200, 200);
+  let canvas = createCanvas(400, 400, WEBGL);
+  
   canvas.mousePressed(userStartAudio);
   userStartAudio();
   microfono = new p5.AudioIn();
@@ -13,14 +16,19 @@ function draw() {
   let gritoNivel = microfono.getLevel();
   // Ajusta la visibilidad de "Grita" en función del nivel de audio
   mostrarTexto = gritoNivel<=0.1
+  text("activar",width / 2,20 );
  
 
   // Ajusta el tamaño de "Grita" en función del nivel de audio
   let tamanioTexto = map(gritoNivel, 0, 1, 1, 10); // Tamaño se ajusta de 1 a 0
   document.getElementById("grita").style.fontSize = tamanioTexto + "em";
 
+
+
   // Ajusta la visibilidad del texto "Grita"
   document.getElementById("frase").style.visibility = mostrarTexto 
     ? "hidden"
     : "visible";
+
+  sleep(1000)
 }
