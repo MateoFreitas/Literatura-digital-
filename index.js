6202f0f2a4a2285dc5dd3de21349f0144a252374
Mateo Freitas <mateofreitas027@gmail.com>
2024-03-21 18:49:39 -0300

    push marzo

--- index.js
+++ index.js
@@ -3,6 +3,17 @@ let microfono;
 let mostrarTexto = true;
 let tamanioGrita = tamanioMaximo;
 let tamanioFrase = 0;
+const frases = [
+"grita hasta que los jubilados no tengan hambre", 
+"grita hasta que todos puedan comer",  
+"grita hasta que dejen de avanzar sobre la educacion publica", 
+"grita hasta que respeten tus derechos"]
+
+const esperando = 0
+const gritando = 1 
+const silencio = 2
+let estado = esperando 
+let fraseActual = frases[Math.floor(Math.random() * frases.length)];
 
 function setup() {
   let canvas = createCanvas(400, 400, WEBGL);
@@ -12,6 +23,8 @@ function setup() {
   microfono = new p5.AudioIn();
   microfono.start();
   frameRate(60);
+  document.getElementById("frase").innerText = fraseAleatoria;
+  
 }
 
 function draw() {
@@ -19,6 +32,19 @@ function draw() {
   // Ajusta la visibilidad de "Grita" en función del nivel de audio
   mostrarTexto = gritoNivel > 0.1;
   text("activar", width / 2, 20);
+
+  if (estado === esperando) {
+    fraseActual = frases[Math.floor(Math.random() * frases.length)];
+  } 
+  if (estado===silencio) {
+    fraseActual = frases[Math.floor(Math.random() * frases.length)];
+    document.getElementById("frase").innerText = frases;
+}
+  }
+  if (estado === gritando){
+    document.getElementById("frase").innerText = frases;
+
+  }
 
   if ((tamanioFrase < tamanioMaximo) & mostrarTexto) {
     // Ajusta el tamaño de "Grita" en función del nivel de audio
@@ -36,4 +62,4 @@ function draw() {
       tamanioGrita > tamanioMaximo ? tamanioMaximo : tamanioGrita + 1;
     document.getElementById("grita").style.fontSize = tamanioGrita + "em";
   }
-}
+
